82831d3e7a0606590866c074c104c99b2279696e
AamirCode2 <aamirlovesart@gmail.com>
2025-10-12 11:00:18 +1000

    Add files via upload

---
+++ Age counter.py
@@ -0,0 +1,9 @@
+try:
+    age = int(input("Enter your age: "))
+    
+    if age%2==0:
+        print("Your age is an even number.")
+    else:
+        print("Your age is an odd number.")
+except ValueError:
+    print("Your input has a ValueError, retry.")
